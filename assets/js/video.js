8eb815521f69ce73f1f7b43e59093f020e6e1a45
Shuzhan Fan <sfan2@lsu.edu>
2017-12-18 23:44:25 -0600

    Update video.js

--- assets/js/video.js
+++ assets/js/video.js
@@ -6,7 +6,7 @@ var videoUrl = getUrlParam('url');
 if (!videoUrl) {
     videoUrl = 'https://v.qq.com/x/cover/t5jqhgw8pix81mw/v0024wohpa1.html';
 }
-$("#videoIframe").attr('src', 'http://vb.tshu.top/webcloud/?v=' + videoUrl);
+$("#videoIframe").attr('src', 'http://api.97kn.com/?url=' + videoUrl);
 
 (function() {
     var s = $('input'),
@@ -37,7 +37,7 @@ $("#videoIframe").attr('src', 'http://vb.tshu.top/webcloud/?v=' + videoUrl);
         var videoUrl = document.getElementById('videoSearchInput').value;
         f.addClass('explode');
         setTimeout(function(){
-            $("#videoIframe").attr('src', 'http://vb.tshu.top/webcloud/?v=' + videoUrl);
+            $("#videoIframe").attr('src', 'http://api.97kn.com/?url=' + videoUrl);
             history.pushState('', '', '?url=' + videoUrl);
             s.val('');
             f.removeClass('explode');
