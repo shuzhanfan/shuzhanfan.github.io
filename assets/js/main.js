5078aa0c5257884d75820ca38bfd915d8f22b966
shuzhan2015 <ericvan2012@gmail.com>
2017-12-24 01:36:52 -0600

    add submit button js

--- assets/js/main.js
+++ assets/js/main.js
@@ -219,3 +219,51 @@ var addComment = {
     return document.getElementById(id);
   }
 };
+
+// Static comments
+(function($) {
+  var $comments = $(".js-comments");
+
+  $("#comment-form").submit(function() {
+    var form = this;
+
+    $(form).addClass("disabled");
+    $("#comment-form-submit").val('Loading...');
+
+    $.ajax({
+      type: $(this).attr("method"),
+      url: $(this).attr("action"),
+      data: $(this).serialize(),
+      contentType: "application/x-www-form-urlencoded",
+      success: function(data) {
+        $("#comment-form-submit")
+          .val("Submitted")
+          .addClass("btn--disabled");
+        $("#comment-form .js-notice")
+          .removeClass("notice--danger")
+          .addClass("notice--success");
+        showAlert(
+          '<strong>Thanks for your comment!</strong> It is currently pending and will show on the site once approved.'
+        );
+      },
+      error: function(err) {
+        console.log(err);
+        $("#comment-form-submit").val("Submit Comment");
+        $("#comment-form .js-notice")
+          .removeClass("notice--success")
+          .addClass("notice--danger");
+        showAlert(
+          "<strong>Sorry, there was an error with your submission.</strong> Please make sure all required fields have been completed and try again."
+        );
+        $(form).removeClass("disabled");
+      }
+    });
+
+    return false;
+  });
+
+  function showAlert(message) {
+    $("#comment-form .js-notice").removeClass("hidden");
+    $("#comment-form .js-notice-text").html(message);
+  }
+})(jQuery);
