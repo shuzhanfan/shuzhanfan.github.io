fb8cf4842964ef9ce1a90611e8216379f0590d66
shuzhan2015 <ericvan2012@gmail.com>
2017-12-23 19:52:04 -0600

    update assets/js/main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -117,51 +117,55 @@ SimpleJekyllSearch.init({
 
 // Staticman comment replies
 // modified from Wordpress https://core.svn.wordpress.org/trunk/wp-includes/js/comment-reply.js
+// Released under the GNU General Public License - https://wordpress.org/about/gpl/
 var addComment = {
-  moveForm: function( commId, parentId, respondId, postId ) {
-    var div, element, style, cssHidden,
-      t           = this,
-      comm        = t.I( commId ),
-      respond     = t.I( respondId ),
-      cancel      = t.I( 'cancel-comment-reply-link' ),
-      parent      = t.I( 'comment-parent' ),
-      post        = t.I( 'comment-post-slug' ),
-      commentForm = respond.getElementsByTagName( 'form' )[0];
+  moveForm: function(commId, parentId, respondId, postId) {
+    var div,
+      element,
+      style,
+      cssHidden,
+      t = this,
+      comm = t.I(commId),
+      respond = t.I(respondId),
+      cancel = t.I("cancel-comment-reply-link"),
+      parent = t.I("comment-replying-to"),
+      post = t.I("comment-post-slug"),
+      commentForm = respond.getElementsByTagName("form")[0];
 
-    if ( ! comm || ! respond || ! cancel || ! parent || ! commentForm ) {
+    if (!comm || !respond || !cancel || !parent || !commentForm) {
       return;
     }
 
     t.respondId = respondId;
     postId = postId || false;
 
-    if ( ! t.I( 'sm-temp-form-div' ) ) {
-      div = document.createElement( 'div' );
-      div.id = 'sm-temp-form-div';
-      div.style.display = 'none';
-      respond.parentNode.insertBefore( div, respond );
+    if (!t.I("sm-temp-form-div")) {
+      div = document.createElement("div");
+      div.id = "sm-temp-form-div";
+      div.style.display = "none";
+      respond.parentNode.insertBefore(div, respond);
     }
 
-    comm.parentNode.insertBefore( respond, comm.nextSibling );
-    if ( post && postId ) {
+    comm.parentNode.insertBefore(respond, comm.nextSibling);
+    if (post && postId) {
       post.value = postId;
     }
     parent.value = parentId;
-    cancel.style.display = '';
+    cancel.style.display = "";
 
     cancel.onclick = function() {
-      var t       = addComment,
-        temp    = t.I( 'sm-temp-form-div' ),
-        respond = t.I( t.respondId );
+      var t = addComment,
+        temp = t.I("sm-temp-form-div"),
+        respond = t.I(t.respondId);
 
-      if ( ! temp || ! respond ) {
+      if (!temp || !respond) {
         return;
       }
 
-      t.I( 'comment-parent' ).value = '0';
-      temp.parentNode.insertBefore( respond, temp );
-      temp.parentNode.removeChild( temp );
-      this.style.display = 'none';
+      t.I("comment-replying-to").value = null;
+      temp.parentNode.insertBefore(respond, temp);
+      temp.parentNode.removeChild(temp);
+      this.style.display = "none";
       this.onclick = null;
       return false;
     };
@@ -172,15 +176,15 @@ var addComment = {
      * 'inherit' when the visibility value is inherited from an ancestor.
      */
     try {
-      for ( var i = 0; i < commentForm.elements.length; i++ ) {
+      for (var i = 0; i < commentForm.elements.length; i++) {
         element = commentForm.elements[i];
         cssHidden = false;
 
         // Modern browsers.
-        if ( 'getComputedStyle' in window ) {
-          style = window.getComputedStyle( element );
-        // IE 8.
-        } else if ( document.documentElement.currentStyle ) {
+        if ("getComputedStyle" in window) {
+          style = window.getComputedStyle(element);
+          // IE 8.
+        } else if (document.documentElement.currentStyle) {
           style = element.currentStyle;
         }
 
@@ -190,12 +194,15 @@ var addComment = {
          * the job for us. In fact, the visibility computed style is the actual
          * computed value and already takes into account the element ancestors.
          */
-        if ( ( element.offsetWidth <= 0 && element.offsetHeight <= 0 ) || style.visibility === 'hidden' ) {
+        if (
+          (element.offsetWidth <= 0 && element.offsetHeight <= 0) ||
+          style.visibility === "hidden"
+        ) {
           cssHidden = true;
         }
 
         // Skip form elements that are hidden or disabled.
-        if ( 'hidden' === element.type || element.disabled || cssHidden ) {
+        if ("hidden" === element.type || element.disabled || cssHidden) {
           continue;
         }
 
@@ -203,13 +210,12 @@ var addComment = {
         // Stop after the first focusable element.
         break;
       }
-
-    } catch( er ) {}
+    } catch (er) {}
 
     return false;
   },
 
-  I: function( id ) {
-    return document.getElementById( id );
+  I: function(id) {
+    return document.getElementById(id);
   }
 };
